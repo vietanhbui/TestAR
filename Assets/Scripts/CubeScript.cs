e221a4677c1ca213a69152825883fbac6773aef0
Anh Bui <anhbuidustin@gmail.com>
2020-05-12 15:22:00 +0700

    first commit

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Disable the behaviour when it becomes invisible...
+    void OnBecameInvisible()
+    {
+        Debug.Log("invisible");
+        enabled = false;
+    }
+
+    // ...and enable it again when it becomes visible.
+    void OnBecameVisible()
+    {
+        Debug.Log("visible");
+        enabled = true;
+    }
+}
